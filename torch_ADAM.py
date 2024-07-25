1c9d3bf93b3a0a382219064ca0f953fab7ae0309
jordigb4 <117774177+jordigb4@users.noreply.github.com>
2024-07-25 13:25:40 +0200

    first_version

--- torch_ADAM.py
+++ torch_ADAM.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 from torch import Tensor
 from torch.optim.optimizer import (Optimizer, _use_grad_for_differentiable, _default_to_fused_or_foreach,
@@ -9,7 +11,14 @@ __all__ = ['adam', 'ADAM']
 
 class Adam(Optimizer):
     def __init__(self, params, lr=1e-3, beta_one=0.9, beta_two=0.999, epsilon=1e-9, *, maximize: bool = False):
-
+        """
+        :param params: specifies which tensors should be optimized
+        :param lr: step size
+        :param beta_one: Exponential decay rate
+        :param beta_two: Exponential decay rate
+        :param epsilon: threshold to consider number as 0
+        :param maximize: bool indicating whether the optimizer should maximize or minimize the objective
+        """
         if lr < 0.0:
             raise ValueError(f"Invalid learning rate: {lr}")
         if beta_one < 0.0:
@@ -18,6 +27,8 @@ class Adam(Optimizer):
             raise ValueError(f"Invalid beta_two: {beta_two}")
         if epsilon < 0.0:
             raise ValueError(f"Invalid epsilon: {epsilon}")
+
+        self.t = 0
 
         defaults = dict(lr=lr, beta_one=beta_one, beta_two=beta_two, epsilon=epsilon, maximize=maximize)
 
@@ -28,7 +39,7 @@ class Adam(Optimizer):
         for group in self.param_groups:
             group.setdefault('maximize', False)
 
-    def _init_group(self, group, params_with_grad, d_p_list, momentum_buffer_list):
+    def _init_group(self, group, params_with_grad, d_p_list, m_0_buffer_list, v_0_buffer_list):
         has_sparse_grad = False
 
         for p in group['params']:
@@ -39,9 +50,81 @@ class Adam(Optimizer):
                     has_sparse_grad = True
 
                 state = self.state[p]
-                momentum_buffer_list.append(state.get('momentum_buffer'))
+
+                m_0_buffer_list.append(state.get('m_0_buffer'))
+                v_0_buffer_list.append(state.get('v_0_buffer'))
 
         return has_sparse_grad
 
     @_use_grad_for_differentiable
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+        for group in self.param_groups:
+            params_with_grad = []
+            d_p_list = []
+            m_0_buffer_list = []
+            v_0_buffer_list = []
+            self.t += 1
+            t = self.t
+            has_sparse_grad = self._init_group(group, params_with_grad, d_p_list,  m_0_buffer_list, v_0_buffer_list)
+
+            adam(params_with_grad, d_p_list, m_0_buffer_list, v_0_buffer_list, t, beta_one=group['beta_one'],
+                 beta_two=group['beta_two'], epsilon=group['epsilon'], lr=group['lr'], maximize=group['maximize'],
+                 has_sparse_grad=has_sparse_grad, grad_scale=getattr(self, "grad_scale", None),
+                 found_inf=getattr(self, "found_inf", None))
+
+            for p, m_0_buffer, v_0_buffer in zip(params_with_grad, m_0_buffer_list, v_0_buffer_list):
+                state = self.state[p]
+                state['m_0_buffer'] = m_0_buffer
+                state['v_0_buffer'] = v_0_buffer
+
+        return loss
+
+
+def adam(params: List[Tensor], d_p_list: List[Tensor], m_0_buffer_list: List[Optional[Tensor]],
+         v_0_buffer_list: List[Optional[Tensor]], t: int, has_sparse_grad: bool = None,
+         grad_scale: Optional[Tensor] = None, found_inf: Optional[Tensor] = None, *, beta_one: float,
+         beta_two: float, lr: float, epsilon: float, maximize: bool):
+
+    func = _single_tensor_adam
+
+    func(params, d_p_list, m_0_buffer_list, v_0_buffer_list, t, beta_one=beta_one, beta_two=beta_two,
+         epsilon=epsilon, lr=lr, has_sparse_grad=has_sparse_grad, maximize=maximize, grad_scale=grad_scale,
+         found_inf=found_inf)
+
+
+def _single_tensor_adam(params: List[Tensor], d_p_list: List[Tensor], m_0_buffer_list: List[Optional[Tensor]],
+                        v_0_buffer_list: List[Optional[Tensor]], t: int, grad_scale: Optional[Tensor],
+                        found_inf: Optional[Tensor], *, beta_one: float, beta_two: float,
+                        lr: float, epsilon: float, maximize: bool, has_sparse_grad: bool):
+
+    assert grad_scale is None and found_inf is None
+
+    for i, param in enumerate(params):
+        d_p = d_p_list[i] if not maximize else -d_p_list[i]
+        m_0_buff = m_0_buffer_list[i]
+
+        if m_0_buff is None:
+            m_0_buff = torch.zeros_like(d_p)
+            m_0_buffer_list[i] = m_0_buff
+        else:
+            m_0_buff.mul_(beta_one).add_(d_p, alpha=1-beta_one)
+
+        v_0_buff = v_0_buffer_list[i]
+        if v_0_buff is None:
+            v_0_buff = torch.zeros_like(d_p)
+            v_0_buffer_list[i] = v_0_buff
+        else:
+            v_0_buff.mul_(beta_two).add_(torch.pow(d_p, 2), alpha=1-beta_two)
+
+        lr_t = lr * ((math.sqrt(1-math.pow(beta_two, t))) / (1 - math.pow(beta_one, t)))
+        param.add_(torch.div(m_0_buff, torch.add(torch.sqrt(v_0_buff),epsilon)), alpha=-lr_t)
+
+
+
+
+
 
